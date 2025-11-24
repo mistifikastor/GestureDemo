962115017e296adcdff70b16c3d58d423c7594ac
mistifikastor <menoftoday@yandex.ru>
2025-11-24 14:54:15 +0700

    Pinch9

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -4,22 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.gestures.rememberTransformableState
 import com.example.gesturedemo.ui.theme.GestureDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,30 +43,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    ScrollModifiers(modifier)
+    MultiTouchDemo(modifier)
 }
 
 @Composable
-fun ScrollModifiers(modifier: Modifier = Modifier) {
-    val image = ImageBitmap.imageResource(id = R.drawable.maxresdefault)
+fun MultiTouchDemo(modifier: Modifier = Modifier) {
+    var scale by remember { mutableStateOf(1f) }
 
-    Box(modifier = modifier
-        .size(150.dp)
-        .verticalScroll(rememberScrollState())
-        .horizontalScroll(rememberScrollState())) {
-        Canvas(
-            modifier = Modifier
-                .size(360.dp, 270.dp)
+    val state = rememberTransformableState {
+            scaleChange, offsetChange, rotationChange ->
+        scale *= scaleChange
+    }
+
+    Box(contentAlignment = Alignment.Center, modifier = modifier.fillMaxSize()) {
+        Box(
+            Modifier
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale
+                )
+                .transformable(state = state)
+                .background(Color.Blue)
+                .size(100.dp)
         )
-        {
-            drawImage(
-                image = image,
-                topLeft = Offset(
-                    x = 0f,
-                    y = 0f
-                ),
-            )
-        }
     }
 }
 
